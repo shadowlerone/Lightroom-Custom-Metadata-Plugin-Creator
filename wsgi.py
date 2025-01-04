3e25edadcbc1de84431ac8f6d89a6d3cc2443b76
shadowlerone <andrae.lewis@gmail.com>
2025-01-04 17:10:52 -0500

    fixed cross origin issues, added logging

--- wsgi.py
+++ wsgi.py
@@ -1,29 +1,48 @@
 
 import shutil
 import pathlib
-
+import logging
 
 import LRPlugin
 
 # from bottle import route, run, post, request, static_file
 from flask import Flask, request, send_file
+from flask_cors import CORS, cross_origin
+
 app = Flask(__name__)
+cors = CORS(app)
+
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
 def hello():
 	return "Hello World!"
 
 @app.post('/generate')
+@cross_origin()
 def generate():
+	logging.info("Received request")
+	# response.headers['Access-Control-Allow-Origin'] = '*'
 	try:
-		plugin = LRPlugin.Plugin.loads(request.json)
+		logging.info("Generating plugin from json request")
+		data = request.json
+		logging.info(data)
+		plugin = LRPlugin.Plugin._l(data)
+		logging.info("Exporting plugin")
 		fp = plugin.export()
+		logging.info("Zipping plugin")
 		zip_name = f"{fp}.zip"
-		shutil.make_archive(zip_name, 'zip', fp)
+		shutil.make_archive(fp, 'zip', fp)
+		logging.info("Sending Plugin")
 		# return static_fi le(zip_name, root="tmp", download=True)
-		return send_file(path_or_file=zip_name,as_attachment=True)
+		response = send_file(path_or_file=zip_name,as_attachment=True)
+		return response
 	except:
+		logging.exception("An Error Occurred")
 		return "An Error Occurred"
+
+
+logging.basicConfig(level=logging.INFO)
 
 # if __name__ == '__main__':
 # 	run(host='localhost', port=8080, debug=True)
