cdae97190b16e6deba0cabfce6ab54463d6a8573
shadowlerone <andrae.lewis@gmail.com>
2025-01-07 08:22:00 -0500

    can delete values in enums

--- lightroom metadata plugin creator/src/components/EnumValueField.vue
+++ lightroom metadata plugin creator/src/components/EnumValueField.vue
@@ -1,20 +1,20 @@
 <template>
-	<div class="field has-addons">
+	<div class="field has-addons" :class="{'has-danger': error}">
 		<div class="control">
 			<div class="control is-expanded">
-				<input :class="{ 'is-danger': (title == '') }" required @change="update_title($event)" class="input"
+				<input :class="{ 'is-danger': (title == '') }" required @change="update_title()" class="input"
 					type="text" v-model="title" placeholder="Title">
 			</div>
 		</div>
 		<div class="control" v-if="advanced">
-				<div class="control">
-					<input :class="{ 'is-danger': (value == '') }" required @change="update_value($event)" class="input"
-						type="text" v-model="value" placeholder="value">
-				</div>
+			<div class="control">
+				<input :class="{ 'is-danger': (value == '') }" required @change="update_value()" class="input"
+					type="text" v-model="value" placeholder="value">
+			</div>
 		</div>
 		<div class="control">
 			<div class="control">
-				<button type="button" class="button is-danger" @click.prevent="$emit('deleteValue')">Delete
+				<button type="button" class="button is-danger" @click.prevent="$emit('delete_value', index)">Delete
 					Value</button>
 			</div>
 		</div>
@@ -24,27 +24,43 @@
 </template>
 
 <script setup>
-import { ref, computed } from 'vue';
+import { ref, computed, onMounted, onBeforeUpdate, onBeforeMount } from 'vue';
 import { to_id } from '../utils.js'
 
-const props = defineProps({ 'index': Number, 'advanced': Boolean })
+const props = defineProps({ 'index': Number, 'advanced': Boolean, 'uuid': Number })
+const emits = defineEmits('delete_value')
 
 let id_default = ref(true)
-let title = ref(`Value ${props.index}`)
+let title = ref(`Value ${props.uuid}`)
 let value = ref(to_id(title.value))
 let model = defineModel({})
-// let errors = defineModel('errors')
+let uuid = ref(props.uuid)
 let error = computed(() => {
-	return (value.value == "" || title.value == "")
+	return (value.value === "" || title.value === "")
 })
+let data = ref({
+	'title': title,
+	'value': value,
+	'error': error,
+	'uuid': uuid
+})
+
 let computed_id_string = computed(() => {
 	return to_id(title.value)
 })
-model.value[props.index] = {
-	'title': title,
-	'value': value,
-	'error': error
+
+function setModel() {
+	model.value[props.index] = data.value
 }
+//  Loads parent's data, copying it to internal data
+function updateModel() {
+	data.title = model.value[props.index].title
+	data.value = model.value[props.index].title
+	data.error = model.value[props.index].error
+}
+onBeforeMount(() => { setModel() })
+
+onBeforeUpdate(() => { updateModel() })
 
 function update_title() {
 	title.value = title.value.trim()
--- lightroom metadata plugin creator/src/components/MetaField.vue
+++ lightroom metadata plugin creator/src/components/MetaField.vue
@@ -1,26 +1,27 @@
 <template>
-	<div class="metafield panel" :class="{ 'is-danger': error }">
+	<div class="metafield panel" :class="{ 'is-danger': error == true }">
 		<p class="panel-heading">{{ title_string || `New Field ${index + 1}` }}
-			<template v-if="advanced == true">
+			<template v-if="advanced">
 				({{ pluginId }}.{{ id_string }})
 			</template>
 		</p>
 		<div class="panel-block">
 			<div class="control">
-				<div class="field" :class="{'is-grouped': advanced}">
+				<div class="field" :class="{ 'is-grouped': advanced }">
 					<label class="label">
 						Title
 						<div class="control is-expanded">
 							<input required @change="update_title()" class="input" type="text"
-								:class="{ 'is-danger': title_string == '' }" placeholder="title" v-model="title_string">
-								<p class="help is-danger" v-if="title_string == ''">A blank title is not permitted</p>
+								:class="{ 'is-danger': title_string === '' }" placeholder="title"
+								v-model="title_string">
+							<p class="help is-danger" v-if="title_string === ''">A blank title is not permitted</p>
 						</div>
 					</label>
 					<template v-if="advanced">
 						<label class="label">
 							ID
 							<div class="control is-expanded">
-								<input :class="{ 'is-danger': id_string == '' }" required @change="update_id()"
+								<input :class="{ 'is-danger': id_string === '' }" required @change="update_id()"
 									class="input" type="text" placeholder="id" v-model="id_string">
 							</div>
 						</label>
@@ -46,23 +47,23 @@
 				<!-- DataType -->
 				<div class="field">
 					<label class="label">
-						DataType: {{ dataType }}
+						Data Type
 					</label>
 					<div class="select">
 						<select v-model="dataType">
-							<option selected="selected" value="string">String</option>
+							<option selected="selected" value="string">{{ advanced ? 'String' : 'Text' }}</option>
 							<option value="url">Url</option>
-							<option value="enum">Enum</option>
+							<option value="enum">{{ advanced ? 'Enum' : 'List' }}</option>
 						</select>
 					</div>
 				</div>
 			</div>
 			<div class="control" v-if="advanced">
-				<pre><code>{{ JSON.stringify(model[index], null, 4) }}</code></pre>
+				<!-- <pre><code>{{ JSON.stringify(model[index], null, 4) }}</code></pre> -->
 			</div>
 		</div>
 
-		<template v-if="dataType == 'enum'">
+		<template v-if="dataType === 'enum'">
 			<div class="panel-block">
 				<div class="control">
 					<div class="field">
@@ -78,9 +79,27 @@
 						</label>
 					</div>
 					<div class="control">
+						<div class="field has-addons">
+							<div class="control">
+								<div class="control is-expanded">
+									<input class="input has-text-centered" readonly type="text" value="Title">
+								</div>
+							</div>
+							<div class="control" v-if="advanced">
+								<div class="control">
+									<input class="input has-text-centered" readonly type="text" value="Value">
+								</div>
+							</div>
+						</div>
 						<EnumValueField :advanced="advanced" :index="i + 1" v-model="values"
-							v-for="(v, i) in values.slice(1)" :key="v.value" />
-						<button @click.prevent="values.push('')" class="button">Add</button>
+							v-for="(v, i) in values.slice(1)" :key="v.uuid" @delete_value="delete_value"
+							:uuid="current_uuid" />
+						<button @click.prevent="add_value()" class="button">Add</button>
+						<p v-if="enum_value_empty" class="help is-danger">{{ advanced ? 'List' : 'Enum' }} has no values.
+						</p>
+						<p v-if="!enum_field_unique_titles" class="help is-danger">All Titles must be unique.</p>
+						<p v-if="!enum_field_unique_values && advanced" class="help is-danger">All values must be
+							unique.</p>
 					</div>
 				</div>
 				<template v-if="advanced">
@@ -116,24 +135,38 @@ let title_string = ref(`New Field ${props.uuid + 1}`)
 let allowPluginToSetOtherValues = ref(true)
 
 let noneName = ref('None')
-let values = ref([{ 'title': noneName, 'value': 'nil' }])
+let values = ref([{ 'title': noneName, 'value': 'nil', 'uuid': -1 }, {'uuid': 0}])
 let id_string = ref(to_id(title_string.value))
 
 let uuid = ref(props.uuid)
+let current_uuid = ref(0)
 const computed_id_string = computed(() => {
 	return to_id(title_string.value)
 })
 const title_error = computed(() => {
-	return title_string.value == ''
+	return title_string.value === ''
 })
 const id_error = computed(() => {
-	return id_string.value == ''
+	return id_string.value === ''
+})
+const enum_value_empty = computed(() => {
+	return (values.value.length <= 1)
+})
+const enum_field_error = computed(() => {
+	return values.value.some(e => e.error)
+})
+const enum_field_unique_values = computed(() => {
+	return (new Set(values.value.map(e => e.value))).size == values.value.length;
+})
+const enum_field_unique_titles = computed(() => {
+	return (new Set(values.value.map(e => e.title))).size == values.value.length;
 })
 const enum_error = computed(() => {
-	var empty = (values.value.length == 0 && dataType.value == 'enum')
-	var field_error = dataType.value == 'enum' && values.value.some(e => e.error)
-	return empty || field_error
-
+	if (dataType.value === 'enum') {
+		return (enum_value_empty.value || enum_field_error.value || !enum_field_unique_values.value || !enum_field_unique_titles.value)
+	} else {
+		return false
+	}
 })
 
 const error = computed(() => {
@@ -168,7 +201,7 @@ function updateModel() {
 	dataType.value = model.value[props.index].dataType
 	values.value = model.value[props.index].values
 	allowPluginToSetOtherValues.value = model.value[props.index].allowPluginToSetOtherValues
-	error.value = model.value[props.index].error
+	// error.value = model.value[props.index].error
 }
 
 // setModel()
@@ -184,13 +217,13 @@ function update_title() {
 }
 
 function update_id_from_title() {
-	if (id_default.value == true) {
+	if (id_default.value) {
 		id_string.value = computed_id_string.value
 	}
 }
 function update_id() {
 	id_string.value = to_id(id_string.value)
-	if (id_string.value == "") {
+	if (id_string.value === "") {
 		id_default.value = true
 		update_id_from_title()
 	} else { id_default.value = false }
@@ -204,6 +237,13 @@ function update_searchable() {
 		browsable_disabled.value = false
 		browsable.value = true
 	}
+}
+
+function add_value() {
+	values.value.push({ 'uuid': current_uuid.value++ })
+}
+function delete_value(index) {
+	values.value.splice(index, 1)
 }
 </script>
 
